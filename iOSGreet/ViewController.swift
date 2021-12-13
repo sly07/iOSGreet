b582a5a7a731bf938f70f60c6510d7a39d3b749b
Mike Sly <mike.sly@vivint.com>
2021-12-13 14:27:50 -0700

    Initial Commit

---
+++ iOSGreet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSGreet
+//
+//  Created by Michael Sly on 12/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
